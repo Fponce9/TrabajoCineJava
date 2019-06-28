c3384f110801a3cb9603929dd4d3e194f2eb66f9
Fernando <exminaruto@gmail.com>
2019-06-27 19:21:29 -0500

    Primer Commit

---
+++ TFCinema/src/main/java/Datos/DatosButacas.java
@@ -0,0 +1,43 @@
+package Datos;
+
+import Entidades.Butaca;
+import Entidades.Funcion;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DatosButacas extends BibliotecaDB
+{   
+    public void ReservarButaca(Butaca oButaca) throws SQLException, ClassNotFoundException, Exception
+    {
+        if (oButaca != null)
+        {            
+            String sql = "INSERT INTO Butacas (IdFuncion, Fila, Columna, Estado) VALUES ('"
+                    + oButaca.getoFuncion().getIdFuncion() + "','"
+                    + oButaca.getFila() + "','"
+                    + oButaca.getColumna() + "','"
+                    + 0 + "')";
+            Conectar();
+            EjecutarSentencia(sql);
+            Desconectar();
+        }
+    }
+       
+    public Boolean ButacaReservada(Funcion oFuncion, int Fila, int Columna) throws SQLException, ClassNotFoundException, Exception
+    {
+        Boolean rta = false;
+        String sql = "SELECT * FROM Butacas WHERE Fila = " + Fila + " AND Columna = " + Columna + " AND idFuncion = " + oFuncion.getIdFuncion() + " AND Estado = 0";
+        Conectar();
+        PreparedStatement s = CrearSentencia(sql);
+        ResultSet Resultado = Consultar(s);
+        
+        while(Resultado.next())
+        {
+            rta = true;
+        }
+        
+        Desconectar();
+        
+        return rta;
+    }   
+}
